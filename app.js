30933efb34ad9d7c6734f34b6dff2480aa7b1098
deepakbhattmits <deepakbhattmits@gmail.com>
2021-08-14 14:32:34 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var userInput;
-var userName;
-userInput = 20;
-userInput = 'Deepak';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-var generateError = function (message, code) { throw { message: message, statusCode: code }; };
-generateError('something went wrong', 500);

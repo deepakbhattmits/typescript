2b98448c4c4d3f29bb430c44b672a9ca43312bb6
deepakbhattmits <deepakbhattmits@gmail.com>
2021-08-21 11:43:24 +0530

    Update functions.js

--- dist/functions.js
+++ dist/functions.js
@@ -1,18 +1,18 @@
 "use strict";
-var sum = function (n1, n2) { return n1 + n2; };
-var printResult = function (num) { return console.log('TEST : ', num); };
-var sumOfTwo = sum(2, 3);
-(function () { printResult(sumOfTwo); })();
+const sum = (n1, n2) => n1 + n2;
+const printResult = (num) => console.log('TEST : ', num);
+const sumOfTwo = sum(2, 3);
+(() => { printResult(sumOfTwo); })();
 // here we got this will hold function only 
 // but there is a issue this will hold any kind of function
 // so we should be more specific 
 // let sumFunction: Function;
-var sumFunction;
+let sumFunction;
 sumFunction = sum;
 // sumFunction = printResult
 console.log('test :', sumFunction(5, 9));
-var addAndHandle = function (n1, n2, cb) {
-    var result = n1 + n2;
+const addAndHandle = (n1, n2, cb) => {
+    const result = n1 + n2;
     cb(result);
 };
-addAndHandle(10, 20, function (result) { return console.log('RESULT : ', result); });
+addAndHandle(10, 20, (result) => console.log('RESULT : ', result));

79d0ba35f3f255fa0c1bfec38409dbae88b836f1
deepakbhattmits <deepakbhattmits@gmail.com>
2021-08-21 11:45:02 +0530

    Update type-casting.js

--- dist/type-casting.js
+++ dist/type-casting.js
@@ -3,10 +3,10 @@
 // const i2 = <HTMLInputElement>document.querySelector('#num2')!;
 // const bu = <HTMLButtonElement>document.querySelector('#button')! ;
 // const i1 = document.querySelector('#str')! as HTMLInputElement;
-var i1 = document.querySelector('#str');
-var i2 = document.querySelector('#num1');
-var i3 = document.querySelector('#num2');
-var bu = document.querySelector('#button');
+const i1 = document.querySelector('#str');
+const i2 = document.querySelector('#num1');
+const i3 = document.querySelector('#num2');
+const bu = document.querySelector('#button');
 //if we don't wan't to use escalamation mark here 
 if (i1) {
     i1.value = 'Deepak';
